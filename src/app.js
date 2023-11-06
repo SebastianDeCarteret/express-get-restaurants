f61d362d146f32a06a84e31bc3f19d75c06b9e2f
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-11-06 13:21:06 +0000

    done up to updating a resturant

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
+const seed = require("../seed");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
+db.sync({ force: true });
+seed();
 
+app.get("/restaurants", async (request, response) => {
+  response.json(await Restaurant.findAll());
+});
 
+app.get("/restaurants/:id", async (request, response) => {
+  response.json(await Restaurant.findByPk(Number(request.params.id)));
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get(
+  "/restaurants/add/:name/:location/:cusine",
+  async (request, response) => {
+    await Restaurant.create({
+      name: request.params.name,
+      location: request.params.location,
+      cuisine: request.params.cusine,
+    });
+    response.status(201).send("Created new resturant");
+  }
+);
+
+app.get(
+  "/restaurants/update/:id/:name?/:location?/:cusine?",
+  async (request, response) => {
+    const resturant = await Restaurant.findByPk(request.params.id);
+    await resturant.update({
+      name: request.params.name,
+      location: request.params.location,
+      cuisine: request.params.cusine,
+    });
+    response.status(201).send("Updated resturant");
+  }
+);
 
 module.exports = app;
